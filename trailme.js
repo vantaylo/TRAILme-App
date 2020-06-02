37bb12611cd2e48e205b2d926768277d2650663d
Henri Houlihan <henri.houlihan@gmail.com>
2020-06-01 21:17:01 -0500

    weather api is called and useful data is stored in arrays

--- trailme.js
+++ trailme.js
@@ -1,5 +1,24 @@
 // $(document).ready(function ())
 
+    // Here we are building the URL we need to query the database
+    var weatherarray = [];
+    var temparray = [];
+    // Here we run our AJAX call to the OpenWeatherMap API
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/onecall?lat=30.2672&lon=-97.7431&exclude=daily&appid=76177e785f18f1154ee3d5f16bf4b076",
+      method: "GET"
+    })
+      // We store all of the retrieved data inside of an object called "response"
+      .then(function(response) {
+       // console.log(response.hourly[4]);
+        for(var i = 0; i < 5; i++){
+        //console.log(response.hourly[i].weather);
+        weatherarray.push(response.hourly[i].weather);
+        temparray.push(response.hourly[i].temp);
+        console.log(weatherarray[i]);
+        console.log(temparray[i]);
+        }
+      });
 //create 5 variables( current hour + next for hours)
 
 // ajax/promise GET method
