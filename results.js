a80585dc095f2b72015f49157aa18ce7b92442a0
Steve Munoz <stevomunoz@yahoo.com>
2020-06-02 19:15:01 -0500

    linked correct files to results.js, add Jquery files,Materiliaze, and google font. Tested calling Hiking Project API to get information and was successfull in receving a response

--- results.js
+++ results.js
@@ -1,11 +1,41 @@
-// $(document).ready(function ())
+// START $(document).ready(function())
+$(document).ready(function () {
+  getTrailName();
+});
+// ^^ END $(document).ready(function())
+function getTrailName(trailName) {
+  var hikingAPI = "https://www.hikingproject.com/data/get-trails?lat=";
+  var apiKey = "200774823-429dcd20b5a00db1d5c1120bef4c2278";
 
-// ajax/promise GET method
+  // hardcoding lat long for Austin and a max distance of 50 miles(for now)
+  var lat = 30.266666;
+  var long = -97.73333;
+  var maxDist = 50;
 
-// get hiking API info
+  var hikingQueryURL =
+    hikingAPI +
+    lat +
+    "&lon=" +
+    long +
+    "&maxDistance=" +
+    maxDist +
+    "&key=" +
+    apiKey;
+
+  //console.log("Hiking URL", hikingQueryURL);
+
+  // get hiking API info
+  $.ajax({
+    url: hikingQueryURL,
+    method: "GET",
+  }).then(function (response) {
+    var trailName = response.trails;
+    //console.log("Response", response);
+    //console.log("Trail Name: ", trailName);
+    //$("#trail-name").append(trailName);
+  });
+}
 
 // append 10 results at a time (nice to have: give user option on how many trails to show at once)
 
 // create Elements,classes,ID's,attributes and append to html
-
-// end $(document).ready(function())
